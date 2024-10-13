7f830374aec6eb751e29e19612bf568d24d1b477
Flanky-dev <hidenobu.akazawa@gmail.com>
2024-10-13 15:40:57 +0900

    最終挙動確認_修正１

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-Rails.application.routes.draw do
+RRails.application.routes.draw do
   devise_for :users
+  # トップページをitems#indexに設定
   root to: 'items#index'
-  resources :items, only: [:new, :create, :edit, :update, :show, :index, :destroy]
-  resources :items do
+
+  # itemsリソースのネストされたordersリソース
+  resources :items, only: [:new, :create, :edit, :update, :show, :index, :destroy] do
     resources :orders, only: [:index, :create]
-  root to: 'orders#index'
-  resources :orders, only:[:index, :create]
   end
 end
 
